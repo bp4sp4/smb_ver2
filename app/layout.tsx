381311b9b48696aca4a2c7cb93f8f27338ef7f30
박상훈 <bp4sp4@naver.com>
2026-01-27 16:31:46 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16772639814"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16772639814');`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{
